198cdbde916947721b37bdb26005cd691efdcc44
LeiYang <2443638666@qq.com>
2017-04-28 14:33:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a24436/myapplication/AP.java
@@ -0,0 +1,12 @@
+package com.example.a24436.myapplication;
+
+/**
+ * AP（access point)点只包含 SSID,level
+ * 接入点(Access Point,AP) 例如路由器
+ * @author Guangliang Liu
+ *
+ */
+public class AP {
+	String SSID;
+	int level;
+}
---
+++ app/src/main/java/com/example/a24436/myapplication/Point.java
@@ -0,0 +1,12 @@
+package com.example.a24436.myapplication;
+
+import java.util.ArrayList;
+
+
+
+
+public class Point {
+	ArrayList<AP> aps = new ArrayList<AP>();
+	int x;
+	int y;
+}
